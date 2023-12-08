3abc12c100e351375e275ea9ce86cae91174dd67
abedalrahman alkayyali <202110142@stu.meu.edu.jo>
2023-12-09 00:27:06 +0300

    add function to convert number to txt

--- V.02/main.cpp
+++ V.02/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <vector>
 #include "Interface_DateTimeSettingType.h"
 #include "Interface_AccountingType.h"
 
+std::string convertToText(int number);
 bool cheack_id_pass(std::string, std::string);
 void displayScreen();
 
@@ -198,4 +200,53 @@ void displayScreen() {
     std::cout << "3- for show static screen " << std::endl;
     std::cout << "4- for show rating option " << std::endl;
     std::cout << "5- for Exit " << std::endl;
-} 
+}
+
+std::string convertToText(int number) {
+    if (number == 0) {
+        return "zero";
+    }
+
+    const char* words1to9[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    const char* words10to19[] = {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    const char* words20to90[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+    std::vector<std::string> parts;
+
+    int thousands = number / 1000;
+    if (thousands > 0) {
+        parts.push_back(convertToText(thousands) + " thousand");
+        number %= 1000;
+    }
+
+    int hundreds = number / 100;
+    if (hundreds > 0) {
+        parts.push_back(std::string(words1to9[hundreds]) + " hundred");
+        number %= 100;
+    }
+
+    if (number >= 10 && number <= 19) {
+        parts.push_back(words10to19[number - 10]);
+    } else {
+        int tens = number / 10;
+        int ones = number % 10;
+        if (tens > 0) {
+            parts.push_back(words20to90[tens]);
+        }
+        if (ones > 0) {
+            parts.push_back(words1to9[ones]);
+        }
+    }
+
+    // Combine the parts into a single string
+    std::string result;
+    for (const std::string& part : parts) {
+        result += part + " ";
+    }
+
+    // Remove trailing space
+    result.pop_back();
+
+    return result;
+}
+
