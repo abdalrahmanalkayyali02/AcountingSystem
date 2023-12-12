2fbf3a7784fcc61e2b1729946081a4c2d5135957
abdalrahman alkayyali <202110142@stu.meu.edu.jo>
2023-12-12 11:03:07 +0300

    fixed the error in the productType constcuter

--- V.02/Impl_productType.cpp
+++ V.02/Impl_productType.cpp
@@ -145,7 +145,7 @@ void productType::print() const {
 }
 
 productType::productType() {
-   /set_productName(" ");
+   set_productName(" ");
     set_productDiscribtion(" ");
     set_productQuantity("0");
     set_productPrice("0"); 
